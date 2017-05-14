4233c5822637c9134e38dc7a9377b5a8cf8acecc
Aminul <aminulaust@gmail.com>
2017-05-14 20:20:14 +0600

    first commit

---
+++ app/src/main/java/aminulaust/com/wowfacebook/MainActivity.java
@@ -0,0 +1,29 @@
+package aminulaust.com.wowfacebook;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private com.facebook.login.widget.LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+
+
+        if (fragment == null) {
+            fragment = new FacebookFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+    }
+    }
+

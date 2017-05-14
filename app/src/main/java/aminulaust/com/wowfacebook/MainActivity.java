b125d0f3395e71c6fc8c9c26d8b20d7896071947
Aminul <aminulaust@gmail.com>
2017-05-14 21:11:05 +0600

    To generate Key Hash --

--- app/src/main/java/aminulaust/com/wowfacebook/MainActivity.java
+++ app/src/main/java/aminulaust/com/wowfacebook/MainActivity.java
@@ -1,9 +1,17 @@
 package aminulaust.com.wowfacebook;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     private com.facebook.login.widget.LoginButton loginButton;
@@ -12,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //----To generate Key Hash --
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "aminulaust.com.wowfacebook",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+//----
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
